8046c4540d9a1735bcad7ffa7857ef60ab5dfac3
Unknown <pierreleberre97@gmail.com>
2019-01-20 00:25:01 +0100

    Model Lampe

---
+++ 24hFarfadet/src/main/java/com/hcode/hFarfadet/Model/Lampe.java
@@ -0,0 +1,48 @@
+package com.hcode.hFarfadet.Model;
+
+public class Lampe {
+    private String nom;
+    private int red;
+    private int green;
+    private int blue;
+
+
+    public Lampe(String nom, int red, int green, int blue) {
+        this.nom = nom;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+}
